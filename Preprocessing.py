e9ef4ddb91722dc34b343fb0f574d1277b6b7a28
SauryStand <voyager2511@outlook.com>
2019-11-21 14:47:47 +0000

    update: preprocess

--- Preprocessing.py
+++ Preprocessing.py
@@ -5,24 +5,30 @@ import numpy as np
 pd.set_option('display.width', 1000)  # 设置字符显示宽度
 pd.set_option('display.max_rows', None)  # 设置显示最大行
 
-with open("dataset/mediaeval-2015-trainingset.txt",'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    lineList = []
-    lines.remove(lines[0])
-    for line in lines:
-        line = line.strip().split("\t")
+def generateRawCSV(filename):
 
-        lineList.append(line)
+    with open(filename,'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        lineList = []
+        lines.remove(lines[0])
+        for line in lines:
+            line = line.strip().split("\t")
 
-        #source.write(line[0]+'\n')
-        #target.write(line[1]+'\n')
-#tweetId	tweetText	userId	imageId(s)	username	timestamp	label
-columns = ['tweetId','tweetText','userId', 'imageId(s)', 'username', 'timestamp', 'label']
-df = pd.DataFrame(lineList, columns=columns)
+            lineList.append(line)
 
-print(df.head())
+            #source.write(line[0]+'\n')
+            #target.write(line[1]+'\n')
+    #tweetId	tweetText	userId	imageId(s)	username	timestamp	label
+    columns = ['tweetId','tweetText','userId', 'imageId(s)', 'username', 'timestamp', 'label']
+    df = pd.DataFrame(lineList, columns=columns)
 
-df.to_csv('dataset/raw_training.csv')
+    print(df.head())
+
+    df.to_csv('dataset/raw_training.csv')
 
 #source.close()
 #target.close()
+
+if __name__ == '__main__':
+    filename = 'dataset/mediaeval-2015-trainingset.txt'
+    generateRawCSV(filename)
